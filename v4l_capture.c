70aeacc90fd8bba3f2b5442e4dd489699613511f
Bastian Ruppert <info@bastian-ruppert.de>
2011-02-18 10:47:18 +0100

    LGPL license note added Signed-off-by: Bastian Ruppert <info@bastian-ruppert.de>

--- v4l_capture.c
+++ v4l_capture.c
@@ -1,7 +1,20 @@
 /*
- *  V4L2 video capture example
- *
- *  This program can be used and distributed without restrictions.
+  Copyright 2010, 2011 Bastian Ruppert.
+  based on the V4L2 video capture example
+ 
+ This library is free software: you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation, either
+ version 3 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
  */
 
 #include <stdio.h>
